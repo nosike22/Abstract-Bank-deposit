510556a43d1cb2cbb47dc083b3c17a4190a219fc
nosike22 <frank.nosike@gmail.com>
2022-02-15 12:50:00 +0100

    Abstract File

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿
+// C# program to show the
+// working of abstract class
+using System;
+
+namespace Frank_s_ { }
+
+
+public abstract class Bank
+{
+
+	public abstract void getBalance();
+
+}
+
+
+public class BankA : Bank
+{
+
+	
+	public override void getBalance()
+	{
+		Console.WriteLine("$100 Was deposited into BankA");
+	}
+}
+
+
+public class BankB : Bank
+{
+
+
+	public override void getBalance()
+	{
+		Console.WriteLine("$150 was deposited into bankB");
+	}
+}
+
+public class BankC : Bank
+{
+
+	
+	public override void getBalance()
+	{
+		Console.WriteLine("$200 was deposited into bankC");
+	}
+}
+
+
+
+public class main_method
+{
+
+	
+	public static void Main()
+	{
+
+		
+		Bank g;
+
+		
+		g = new BankA();
+
+		
+		g.getBalance();
+
+		
+		g = new BankB();
+
+	
+		g.getBalance();
+
+		
+		g = new BankC();
+		
+		g . getBalance();
+
+	}
+}
+
+
+
